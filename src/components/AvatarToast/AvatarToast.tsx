52fe4487043e8d58f279c7759ddf8ad0499fa345
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2025-02-05 22:29:42 +0100

    Add speech-bubble, and tune avatar popin

--- src/components/AvatarToast/AvatarToast.tsx
+++ src/components/AvatarToast/AvatarToast.tsx
@@ -2,40 +2,58 @@ import style from "./AvatarToast.module.css";
 import avatar from "../../assets/images/avatar-vector.png";
 import { InView } from "react-intersection-observer";
 import { useEffect, useState } from "react";
+import SpeechBubble from "../SpeechBubble/SpeechBubble";
 
 const AvatarToast = () => {
-  const [isVisible, setIsVisible] = useState(false);
+	const [isVisible, setIsVisible] = useState(false);
 
-  // Retreat avatar after delay
-  useEffect(()=> {
-    let timer: number | undefined; 
-    if(isVisible) {
-      timer = setTimeout(()=> {
-        setIsVisible(false);
-      }, 4000)
-    }
+	// Retreat avatar after delay
+	useEffect(() => {
+		let timer: number | undefined;
+		if (isVisible) {
+			timer = setTimeout(() => {
+				setIsVisible(false);
+			}, 4000);
+		}
 
-    console.log(timer);
-    
-    return ()=> clearTimeout(timer);
-  }, [isVisible])
+		return () => clearTimeout(timer);
+	}, [isVisible]);
 
-	return (    
-		<InView threshold={0.2} triggerOnce={true} onChange={(inView)=> inView && setIsVisible(true)}>
-			{({ ref }) => (
-				<div className={style.wrapper} ref={ref}>          
-					<img
-						className={style.character}
-						src={avatar}
-						alt="avatar of me, popping in to say thanks"
-            // Slide in when in view
-            style={{
-							transform: isVisible ? `translateX(45px) rotate(-45deg)` : `translateX(175px) rotate(0deg)`,
-						}}
-					/>
-				</div>
-			)}
-		</InView>
+	return (
+		<>
+			<InView
+				threshold={0.2}
+				triggerOnce={true}
+				onChange={(inView) => inView && setIsVisible(true)}
+			>
+				{({ ref }) => (
+					<div className={style.wrapper} ref={ref}>
+						<img
+							className={style.character}
+							src={avatar}
+							alt="avatar of me, popping in to say thanks"
+							// Slide in when in view
+							style={{
+								transform: isVisible
+									? `translateX(45px) rotate(-45deg)`
+									: `translateX(175px) rotate(0deg)`,
+							}}
+						/>
+
+						<SpeechBubble
+							speaksFrom="right"
+							style={{
+								opacity: isVisible ? 1 : 0,
+								transition: "opacity 400ms ease-out",
+                transform: "rotate(-10deg)"
+							}}
+						>
+							Thanks for stopping by! <br /> 💙🧡
+						</SpeechBubble>
+					</div>
+				)}
+			</InView>
+		</>
 	);
 };
 
--- src/components/SpeechBubble/SpeechBubble.tsx
+++ src/components/SpeechBubble/SpeechBubble.tsx
@@ -1,17 +1,48 @@
 import React from "react";
-import speechImage from "../../assets/images/speech_bubble.png";
-import style from './SpeechBubble.modules.css';
+import style from "./SpeechBubble.module.css";
 
-type SpeechBubbleProps = {
-	children: React.ReactNode;
-}
+interface SpeechBubbleProps extends React.HTMLAttributes<HTMLDivElement> {
+	children: React.ReactNode;	
+	fillColor?: string;
+	strokeColor?: string;
+	speaksFrom?: "left" | "right";
+};
 
-const SpeechBubble = ({ children }: SpeechBubbleProps) => {
+const SpeechBubble = ({
+	children,
+	fillColor = "transparent",
+	strokeColor = "white",
+	speaksFrom = "left",
+	...delegated
+}: SpeechBubbleProps) => {
+	const isFlipped = speaksFrom === "right" ? true : false;
+
 	return (
 		<>
-			<div className={style.speechBubbleWrapper}>
-				<img src={speechImage} alt="speech bubble" />
-				<div className={style.speechBubbleText}>{children}</div>
+			<div 
+				className={style.speechBubbleWrapper}
+				{...delegated}			
+			>
+				<svg
+					
+					viewBox="0 0 200 150"
+					xmlns="http://www.w3.org/2000/svg"
+					fill={fillColor}
+					stroke={strokeColor}
+					strokeWidth="1px"
+					style={{transform: isFlipped ? "rotateY(180deg)" : "rotate(0)"}}
+				>
+					<path
+						d="M10 40 Q10 10, 40 10 L160 10 Q190 10, 190 40 L190 100 Q190 130, 160 130 L80 130 L40 150 L50 130 Q10 130, 10 100 Z"
+						fill={fillColor}
+						stroke={strokeColor}
+						strokeWidth="1px"
+					/>
+				</svg>
+
+				<div 
+				className={style.speechBubbleText} 
+				>{children}</div>
 			</div>
 		</>
 	);
