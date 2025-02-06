417b69952bd04b16b3c0cbe6eae06f8163f696ee
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2025-02-06 08:46:14 +0100

    Adjust speechBubble stroke

--- src/components/SpeechBubble/SpeechBubble.tsx
+++ src/components/SpeechBubble/SpeechBubble.tsx
@@ -29,14 +29,14 @@ const SpeechBubble = ({
 					xmlns="http://www.w3.org/2000/svg"
 					fill={fillColor}
 					stroke={strokeColor}
-					strokeWidth="1px"
+					strokeWidth="2px"
 					style={{transform: isFlipped ? "rotateY(180deg)" : "rotate(0)"}}
 				>
 					<path
 						d="M10 40 Q10 10, 40 10 L160 10 Q190 10, 190 40 L190 100 Q190 130, 160 130 L80 130 L40 150 L50 130 Q10 130, 10 100 Z"
 						fill={fillColor}
 						stroke={strokeColor}
-						strokeWidth="1px"
+						strokeWidth="2px"
 					/>
 				</svg>
 
