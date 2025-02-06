54b4777a07d5ea3c56e1d1f4f36e0d72f950d66b
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2025-02-06 13:10:24 +0100

    Fix tabbing on technologies section

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Portfolio page - Frontend Developer - Simon Winter
 
-
+Portfolio-page created to display some of my project, and to practice React and Typescript
 
 
 
 
 ## Credits
 * Iconpack: SVGRepos: Dazzle Line Icons
-* 
+ 
--- src/components/Project/ProjectComponent.tsx
+++ src/components/Project/ProjectComponent.tsx
@@ -17,16 +17,14 @@ const ProjectComponent = ({ project, index, images }: ProjectComponentType) => {
 		{({inView, ref}) =>
 			<div ref={ref} className={inView ? `${style.projectContainer} ${style.fadeIn}`: style.projectContainer }>
 				<div className={ inView ? `${style.project__imageContent} ${style.fadeIn}`: style.project__imageContent}>
-					<button>
-						<a href={project.liveUrl} target='_blank' title={project.liveUrl ? 'Link to live view of project': 'No live view available - In private use'}>
-							<img src={images[index]} alt={project.title} />
-						</a>
-					</button>
+					<a href={project.liveUrl} target='_blank' title={project.liveUrl ? 'Link to live view of project': 'No live view available - In private use'}>
+						<img src={images[index]} alt={project.title} />
+					</a>
 				</div>
 				<div className={ inView ? `${style.project__textContent} ${style.fadeIn}`: style.project__textContent}>
 					<h3>{project.title}</h3>
 					<div>{project.technologies}</div>
-					<a href={project.gitUrl} title='Link to code on github' target='_blank'><button>Code on GitHub</button></a>
+					<a href={project.gitUrl} title='Link to code on github' target='_blank'>Code on GitHub</a>
 					<div>
 						<p className={style.projectDescriptionLabel}>Description:</p>
 						<p>{project.description}</p>
